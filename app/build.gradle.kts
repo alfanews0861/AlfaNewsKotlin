9965a4dedef26f2969e86e3541a665d934996c39
alfanews0861 <alfanews0861@gmail.com>
2026-04-04 11:42:49 +0530

    Upgrade app version to Sree_4.7 and versionCode to 567

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.alfanews.telugu"
         minSdk = 24
         targetSdk = 35
-        versionCode = 566
-        versionName = "Sree_4.6"
+        versionCode = 567
+        versionName = "Sree_4.7"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
